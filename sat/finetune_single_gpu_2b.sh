5238de562c37125b038c9b3a0ebf93f4155485c1
glide-the <dmeck@suoxya.com>
2024-09-22 20:43:00 +0800

    Update sft.yaml configuration for finetuning and inference

--- sat/finetune_single_gpu_2b.sh
+++ sat/finetune_single_gpu_2b.sh
@@ -4,7 +4,7 @@ echo "RUN on `hostname`, CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES"
 
 environs="WORLD_SIZE=1 RANK=0 LOCAL_RANK=0 LOCAL_WORLD_SIZE=1"
 
-run_cmd="torchrun --standalone --nproc_per_node=4 train_video.py --base configs/cogvideox_2b_lora.yaml configs/sft.yaml --seed 42"
+run_cmd="torchrun --standalone --nproc_per_node=8 train_video.py --base configs/cogvideox_2b_lora.yaml configs/sft.yaml --seed 42"
 
 echo ${run_cmd}
 eval ${run_cmd}
